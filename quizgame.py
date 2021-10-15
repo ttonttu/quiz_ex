b51d73fef7d7825208b10144b99dc31791eb226a
Elina <elina.ylkanen.gcp@gmail.com>
2021-10-15 14:51:39 +0300

    pelin alkukoodi

--- quizgame.py
+++ quizgame.py
@@ -1,1 +1,33 @@
 #This is the amazing quiz game made by Elina and Miia (and Tiina)
+
+oikein = 0
+kysymys1 = "Mikä on maailman pienin valtio?"
+kysymys2 = "Mikä on maailman pisin joki?"
+kysymys3 = "Kuinka monta raitaa Yhdysvaltain lipussa on?"
+kysymykset = [kysymys1,kysymys2,kysymys3]
+vastaukset = ["Vatikaani", "Niili", "13"]
+kysymys = 3
+j = 0
+
+start = input("Haluatko käynnistää pelin? Y/N: ")
+if start == "Y":
+    while True:
+        for i in kysymykset:
+            kysymykset[j]            
+            vastaus = input("Mikä on maailman pienin valtio? ")
+            if vastaus == vastaukset[j]:
+                oikein +=1
+                j+=1
+            else:
+                pass
+
+
+
+        
+        
+        print(oikein)
+        break
+
+elif start == "N":
+    pass   
+    
